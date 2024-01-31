8d07c53e626abaa866f50fa955d76658aab4b124
VoMinhLoi <minhloi1131130@gmail.com>
2024-01-31 15:18:56 +0700

    Render Brand on Admin interface.

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class ManageController extends Controller
 {
     public function index(){
-        return view('Admin.manage');
+        $brand = Brand::all();
+        return view('Admin.manage',['brand' => $brand]);
     }
 }
